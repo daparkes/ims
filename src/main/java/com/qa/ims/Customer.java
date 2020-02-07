3f87bdb3c3a1953eb41bfe3c345540fbb49f49f1
Dan Parkes <dan.parkes15@gmail.com>
2020-02-07 15:40:02 +0000

    small update

--- src/main/java/com/qa/ims/Customer.java
+++ src/main/java/com/qa/ims/Customer.java
@@ -2,4 +2,13 @@ package com.qa.ims;
 
 public class Customer {
 
+		String firstName;
+		String surname;
+		long id;
+		
+		public Customer (String firstName, String surname, long id) {
+			this.firstName = firstName;
+			this.surname = surname;
+			
+		}
 }
