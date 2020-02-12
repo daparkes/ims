42729e82c22bd4052dd2380242f3af7ac4c3a9b1
Dan Parkes <dan.parkes15@gmail.com>
2020-02-12 16:38:42 +0000

    added package.info files

---
+++ src/main/java/com/qa/ims/persistence/dao/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the data access objects that interact with the database
+ */
+package com.qa.ims.persistence.dao;
---
+++ src/main/java/com/qa/ims/persistence/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the value objects that represent records in the database
+ */
+package com.qa.ims.persistence.domain;
